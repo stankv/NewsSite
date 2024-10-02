478b78e3caddc4cef3bc63add857ebb517786fce
stankv <stankv@mail.ru>
2024-10-02 16:09:30 +0300

    added attribute verbose_name and Meta classes

--- news_site/news_site_app/models.py
+++ news_site/news_site_app/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Рубрика")
 
     def __str__(self):
         return self.name
@@ -12,18 +12,28 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
 
+    class Meta:
+        verbose_name = 'Рубрика'
+        verbose_name_plural = 'Рубрики'
+        ordering = ['id']
+
 
 class Article(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=255, verbose_name="Название")
+    content = models.TextField(blank=True, verbose_name="Содержание")
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name="Изображение")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+    is_published = models.BooleanField(default=True, verbose_name="Опубликовано")
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, verbose_name="Рубрика")
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Статьи'
+        verbose_name_plural = 'Статьи'
+        ordering = ['-time_create', 'title']
