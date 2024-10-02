93c996d5df91988017c6c20e6cf6432e93e2f0f1
stankv <stankv@mail.ru>
2024-10-02 16:12:25 +0300

    added verbose_name to config class

--- news_site/news_site_app/apps.py
+++ news_site/news_site_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class NewsSiteAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'news_site_app'
+    verbose_name = 'Статьи и Категории'
