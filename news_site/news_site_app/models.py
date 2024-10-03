1fe4846ef5ae06b0fbcb797a67def894fa90bc4d
stankv <stankv@mail.ru>
2024-10-03 13:01:35 +0300

    added slug fields to models

--- news_site/news_site_app/models.py
+++ news_site/news_site_app/models.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True, verbose_name="Рубрика")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
 
     def __str__(self):
         return self.name
@@ -20,12 +21,13 @@ class Category(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length=255, verbose_name="Название")
+    slug = models.SlugField(max_length=255, unique=True ,db_index=True, verbose_name="URL")
     content = models.TextField(blank=True, verbose_name="Содержание")
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name="Изображение")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
     is_published = models.BooleanField(default=True, verbose_name="Опубликовано")
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, verbose_name="Рубрика")
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name="Рубрика")
 
     def __str__(self):
         return self.title
