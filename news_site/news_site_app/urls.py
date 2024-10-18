c396cfee5b9f0236946d07e629b92d7f4aa21df9
stankv <stankv@mail.ru>
2024-10-18 18:56:08 +0300

    function View -> class View

--- news_site/news_site_app/urls.py
+++ news_site/news_site_app/urls.py
@@ -2,13 +2,14 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
+    # path('', index, name='home'),    # для вьюхи-функции
+    path('', IndexView.as_view(), name='home'),
     path('about/', about, name='about'),
-    path('addpage/', addpage, name='add_page'),
+    path('addpage/', AddPage.as_view(), name='add_page'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
-    path('post/<slug:post_slug>/', show_post, name='post'),
+    path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
     # path('categories/', categories),
-    path('category/<slug:cat_slug>/', show_category, name='category'),
+    path('category/<slug:cat_slug>/', ArticleCategory.as_view(), name='category'),
     #re_path(r'^archive/(?P<year>[0-9]{4})/', archive)
 ]
--- news_site/news_site_app/views.py
+++ news_site/news_site_app/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
 from .forms import *
 from .models import *
@@ -11,17 +13,41 @@ menu = [{"title":"О сайте", "url_name": "about"},
         {"title":"Войти", "url_name": "login"},
 ]
 
-def index(request):
-    posts = Article.objects.all()
-    cats = Category.objects.all()
-    context = {
-        "posts": posts,
-        "cats": cats,
-        "menu": menu,
-        "title": "Главная страница",
-        "cat_selected":0,
-    }
-    return render(request, 'news_site_app/index.html', context=context)
+#----------------------------------------------------------------------------------------------------------------------
+# представление как функция
+# def index(request):
+#     posts = Article.objects.all()
+#     cats = Category.objects.all()
+#     context = {
+#         "posts": posts,
+#         "cats": cats,
+#         "menu": menu,
+#         "title": "Главная страница",
+#         "cat_selected":0,
+#     }
+#     return render(request, 'news_site_app/index.html', context=context)
+
+
+# представление как класс
+class IndexView(ListView):
+    model = Article
+    template_name = 'news_site_app/index.html'    # указываем путь к нашему шаблону, а не article_list.html
+    context_object_name = 'posts'    # указываем posts для цикла в index.html, а не object_list
+
+    # для передачи в шаблон контекста объекта списка постов menu
+    def get_context_data(self, *, object_list=None, **kwargs):
+        cats = Category.objects.all()
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        context['menu'] = menu
+        context['cats'] = cats
+        context['cat_selected'] = 0    # чтобы в меню пункт Все категории был выбранным ("нажатым"
+        return context
+
+    # чтобы отображались только те статьи, что отмечены для публикации
+    def get_queryset(self):
+        return Article.objects.filter(is_published=True)
+#----------------------------------------------------------------------------------------------------------------------
 
 
 def about(request):
@@ -30,20 +56,36 @@ def about(request):
                                                         "cats": cats, "title": "О сайте"})
 
 
-def addpage(request):
-    if request.method == "POST":
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            # try:
-            #     Article.objects.create(**form.cleaned_data)    # так если форма не связана с моделью
-            #     return  redirect('home')
-            # except:
-            #     form.add_error(None, "Не удалось добавить статью")
-            form.save()  # так если форма связана с моделью, try-except не нужен
-    else:
-        form = AddPostForm()
-    return render(request, 'news_site_app/addpage.html', {"form": form, "menu": menu, "title": "Добавить статью"})
+#----------------------------------------------------------------------------------------------------------------------
+# def addpage(request):
+#     cats = Category.objects.all()
+#     if request.method == "POST":
+#         form = AddPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             # try:
+#             #     Article.objects.create(**form.cleaned_data)    # так если форма не связана с моделью
+#             #     return  redirect('home')
+#             # except:
+#             #     form.add_error(None, "Не удалось добавить статью")
+#             form.save()  # так если форма связана с моделью, try-except не нужен
+#     else:
+#         form = AddPostForm()
+#     return render(request, 'news_site_app/addpage.html', {"cats": cats, "form": form, "menu": menu, "title": "Добавить статью"})
 
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'news_site_app/addpage.html'
+    success_url = reverse_lazy('home')    # адрес куда будет перенаправлен после успешной отправки формы
+                                          # это нужно когда нет функций get_absolute_url()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cats = Category.objects.all()
+        context['title'] = 'Добавление статьи'
+        context['menu'] = menu
+        context['cats'] = cats
+        return context
+#----------------------------------------------------------------------------------------------------------------------
 
 def contact(request):
     return HttpResponse('Обратная связь')
@@ -53,35 +95,72 @@ def login(request):
     return HttpResponse('Авторизация')
 
 
-def show_category(request, cat_slug):
-    cat = get_object_or_404(Category, slug=cat_slug)
-    cats = Category.objects.all()
-    posts = Article.objects.filter(category=cat)
-    if len(posts) == 0:
-        raise Http404
+#----------------------------------------------------------------------------------------------------------------------
+# представление как функция
+# def show_category(request, cat_slug):
+#     cat = get_object_or_404(Category, slug=cat_slug)
+#     cats = Category.objects.all()
+#     posts = Article.objects.filter(category=cat)
+#     if len(posts) == 0:
+#         raise Http404
+#
+#     context = {
+#         "posts": posts,
+#         "cats": cats,
+#         "menu": menu,
+#         "title": f"Новости по категории {cat.name}",
+#         "cat_selected": cat.id,
+#     }
+#     return render(request, 'news_site_app/index.html', context=context)
 
-    context = {
-        "posts": posts,
-        "cats": cats,
-        "menu": menu,
-        "title": f"Новости по категории {cat.name}",
-        "cat_selected": cat.id,
-    }
-    return render(request, 'news_site_app/index.html', context=context)
+# представление как класс
+class ArticleCategory(ListView):
+    model = Article
+    template_name = 'news_site_app/index.html'
+    context_object_name = 'posts'
+    allow_empty = False    # генерирует ошибку 404, если выход за пределы массива posts (или нет такого слага)
 
+    def get_queryset(self):
+        return Article.objects.filter(category__slug=self.kwargs['cat_slug'], is_published=True)
 
-def show_post(request, post_slug):
-    post = get_object_or_404(Article, slug=post_slug)
-    cats = Category.objects.all()
-    context = {
-        "post": post,
-        "cats": cats,
-        "menu": menu,
-        "title": post.title,
-        "cat_selected": post.category_id,
-    }
-    return render(request, 'news_site_app/post.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        cats = Category.objects.all()
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Категория - ' + str(context['posts'][0].category)
+        context['menu'] = menu
+        context['cats'] = cats
+        context['cat_selected'] = context['posts'][0].category_id
+        return context
+#----------------------------------------------------------------------------------------------------------------------
 
+#----------------------------------------------------------------------------------------------------------------------
+# def show_post(request, post_slug):
+#     post = get_object_or_404(Article, slug=post_slug)
+#     cats = Category.objects.all()
+#     context = {
+#         "post": post,
+#         "cats": cats,
+#         "menu": menu,
+#         "title": post.title,
+#         "cat_selected": post.category_id,
+#     }
+#     return render(request, 'news_site_app/post.html', context=context)
+
+class ShowPost(DetailView):
+    model = Article
+    template_name = 'news_site_app/post.html'
+    slug_url_kwarg = 'post_slug'    # указываем наше название вместо slug по умолчанию
+    # pk_url_kwarg = 'post_id'    # указываем наше название вместо id по умолчанию
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cats = Category.objects.all()
+        context['title'] = context['post']
+        context['menu'] = menu
+        context['cats'] = cats
+        return context
+#----------------------------------------------------------------------------------------------------------------------
 
 # обработка несуществующего маршрута (страницы) - возврат страницы 404
 def page_not_found(request, exception):
