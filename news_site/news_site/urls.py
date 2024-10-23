f1333416658ef1fa2833f5c2a6c4907101b4377b
stankv <stankv@mail.ru>
2024-10-23 15:53:19 +0300

    added django-debug-toolbar

--- news_site/news_site/urls.py
+++ news_site/news_site/urls.py
@@ -27,6 +27,9 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+
+    urlpatterns += debug_toolbar_urls()
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 handler404 = page_not_found
