1b317a962303bc1299ece6a8f1871084af1f87df
stankv <stankv@mail.ru>
2024-10-24 14:51:17 +0300

    spelling error fixed

--- news_site/news_site_app/forms.py
+++ news_site/news_site_app/forms.py
@@ -56,6 +56,6 @@ class LoginUserForm(AuthenticationForm):
 
 class ContactForm(forms.Form):
     name = forms.CharField(label='Имя', max_length=255)
-    email = forms.EmailField(label='Email')
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
-    capatcha = CaptchaField()
+    email = forms.EmailField(label='E-mail')
+    content = forms.CharField(label='Сообщение', widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
+    captcha = CaptchaField(label='Введите символы на картинке')
