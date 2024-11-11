89092b225829ba05f8575dbb8b3eaa06254aab15
stankv <stankv@mail.ru>
2024-11-11 13:32:54 +0300

    global settings for DRF added

--- news_site/news_site/settings.py
+++ news_site/news_site/settings.py
@@ -145,3 +145,10 @@ CACHES = {
         "LOCATION": os.path.join(BASE_DIR, "news_site_cache"),
     }
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
