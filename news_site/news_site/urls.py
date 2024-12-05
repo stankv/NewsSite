49394aaf8ccc16f29c0c6e7f11160eb64daa62f8
stankv <stankv@mail.ru>
2024-12-05 16:36:07 +0300

    SimpleRouter added

--- news_site/news_site/urls.py
+++ news_site/news_site/urls.py
@@ -23,7 +23,7 @@ from django.urls import path, include
 from rest_framework import routers
 
 router = routers.SimpleRouter()
-router.register(r'article', ArticleViewSet)
+router.register(r'article', ArticleViewSet, basename='article')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
