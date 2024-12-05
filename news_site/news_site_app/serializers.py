d7e9f16730d86154ea46e02afbbb5a6aef29d1c3
stankv <stankv@mail.ru>
2024-12-05 18:51:34 +0300

    serializer extended

--- news_site/news_site_app/serializers.py
+++ news_site/news_site_app/serializers.py
@@ -4,8 +4,10 @@ from .models import Article
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Article
-        fields = ('title', 'slug', 'content', 'category')
+        fields = ('title', 'slug', 'content', 'category', 'user')
         # fields = '__all__'    # all fields
 
